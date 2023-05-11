890d1f54d490ef543e7a82ba4406847e2090cb21
Thomas Kirst <thomas.kirst@gmail.com>
2023-05-11 12:08:14 +0200

    Hello on the edge

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,7 +1,9 @@
+import { NextResponse } from 'next/server';
+
 export const config = {
   runtime: 'edge',
 };
 
-export default function handler(request, response) {
-  response.status(200).json({ text: 'Hello' });
+export default function handler(request) {
+  return NextResponse.json({ text: 'Hello' });
 }
